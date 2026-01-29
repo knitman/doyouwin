6ed8a16635b5d0a840fc9a50e618b1d9edfae36b
knitman <124901709+knitman@users.noreply.github.com>
2026-01-29 09:23:50 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,17 +21,17 @@ const wss = new WebSocket.Server({ server });
 
 const EMOJIS = ["😎","🤡","🤖","👑","🐱","🐶","🦊","🐸","👻","💀"];
 
-let players = {};
+let players = {}; // id -> { emoji, pos, started }
 let turnOrder = [];
 let currentTurn = 0;
 let gameState = "WAITING";
 
-function broadcast(type, data) {
+function broadcast(type, data){
   const msg = JSON.stringify({ type, data });
   wss.clients.forEach(c => c.readyState === 1 && c.send(msg));
 }
 
-function resetGame() {
+function resetGame(){
   players = {};
   turnOrder = [];
   currentTurn = 0;
@@ -42,57 +42,49 @@ wss.on("connection", ws => {
   ws.id = Math.random().toString(36).slice(2);
 
   ws.send(JSON.stringify({
-    type: "INIT",
-    data: { players, turn: null, gameState, emojis: EMOJIS }
+    type:"INIT",
+    data:{ players, turn:null, gameState, emojis:EMOJIS }
   }));
 
   ws.on("message", msg => {
     const { type, data } = JSON.parse(msg);
 
-    if (type === "JOIN" && gameState === "WAITING") {
-      if (Object.values(players).some(p => p.emoji === data.emoji)) return;
-      players[ws.id] = { emoji: data.emoji, pos: 1, ghost: false };
+    if(type==="JOIN" && gameState==="WAITING"){
+      if(Object.values(players).some(p=>p.emoji===data.emoji)) return;
+      players[ws.id] = { emoji:data.emoji, pos:1, started:false };
       turnOrder.push(ws.id);
-      broadcast("UPDATE", { players, turn: turnOrder[currentTurn], gameState });
+      broadcast("UPDATE",{ players, turn:turnOrder[currentTurn], gameState });
     }
 
-    if (type === "START" && gameState === "WAITING") {
-      if (turnOrder.length === 0) return;
-      gameState = "PLAYING";
-      broadcast("UPDATE", { players, turn: turnOrder[currentTurn], gameState });
+    if(type==="START" && gameState==="WAITING"){
+      if(turnOrder.length===0) return;
+      gameState="PLAYING";
+      broadcast("UPDATE",{ players, turn:turnOrder[currentTurn], gameState });
     }
 
-    if (type === "ROLL" && gameState === "PLAYING") {
-      if (ws.id !== turnOrder[currentTurn]) return;
-      const dice = Math.floor(Math.random() * 6) + 1;
-      broadcast("DICE", { id: ws.id, dice });
+    if(type==="ROLL" && gameState==="PLAYING"){
+      if(ws.id!==turnOrder[currentTurn]) return;
+      const dice = Math.floor(Math.random()*6)+1;
+      broadcast("DICE",{ id:ws.id, dice });
     }
 
-    if (type === "MOVE_DONE" && gameState === "PLAYING") {
+    if(type==="MOVE_DONE"){
       players[data.id].pos = data.pos;
-      if (data.pos >= 100) {
-        gameState = "FINISHED";
-        broadcast("WIN", { emoji: players[data.id].emoji });
-      } else {
-        currentTurn = (currentTurn + 1) % turnOrder.length;
-        broadcast("UPDATE", { players, turn: turnOrder[currentTurn], gameState });
+      players[data.id].started = true;
+      if(data.pos>=100){
+        gameState="FINISHED";
+        broadcast("WIN",{ emoji:players[data.id].emoji });
+      }else{
+        currentTurn=(currentTurn+1)%turnOrder.length;
+        broadcast("UPDATE",{ players, turn:turnOrder[currentTurn], gameState });
       }
     }
 
-    if (type === "RESTART") {
+    if(type==="RESTART"){
       resetGame();
-      broadcast("UPDATE", { players, turn: null, gameState });
-    }
-  });
-
-  ws.on("close", () => {
-    if (players[ws.id]) {
-      players[ws.id].ghost = true;
-      broadcast("UPDATE", { players, turn: turnOrder[currentTurn], gameState });
+      broadcast("UPDATE",{ players, turn:null, gameState });
     }
   });
 });
 
-server.listen(PORT, () =>
-  console.log("🟢 Party Board live on port", PORT)
-);
+server.listen(PORT,()=>console.log("🟢 Party Board live"));
